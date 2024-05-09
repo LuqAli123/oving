0447c768458623c0d072f87cf41a04bccf8d9065
luqman <luali7239@oslomet.no>
2024-05-09 15:46:27 +0200

    filer

---
+++ src/main/java/com/example/oving/OvingApplication.java
@@ -0,0 +1,13 @@
+package com.example.oving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OvingApplication.class, args);
+    }
+
+}
